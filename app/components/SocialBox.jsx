6ad6be36d1bf9ab94a2b9a8ad78c49cd58124c43
Sergey Kibish <sergey.kibish@gmail.com>
2016-07-31 16:47:55 +0300

    Little style for `follow us` section remove ArchivedEventsBox component

--- app/components/SocialBox.jsx
+++ app/components/SocialBox.jsx
@@ -6,14 +6,14 @@ class SocialBox extends React.Component {
     return (
       <div className="social-section">
         <div className="mdl-grid section--center">
-          <div className="mdl-cell mdl-cell--3-col">
+          <div className="mdl-cell mdl-cell--6-col">
             <div className="mdl-typography--display-2 mdl-typography--font-thin">Follow us</div>
               <p className="mdl-typography--headline mdl-typography--font-thin">Be ready for something new</p>
           </div>
-          <div className="mdl-cell mdl-cell--5-col mdl-typography--text-center">
-            <a className="social-link" href={this.props.facebookUrl} target="_blank"><i className="mdi mdi-facebook-box"></i></a>
-            <a className="social-link" href={this.props.twitterUrl} target="_blank"><i className="mdi mdi-twitter-box"></i></a>
-            <a className="social-link" href={this.props.gPlusUrl} target="_blank"><i className="mdi mdi-google-plus-box"></i></a>
+          <div className="mdl-cell mdl-cell--6-col mdl-typography--text-center">
+            <a className="mdl-typography--display-3 social-link" href={this.props.facebookUrl} target="_blank"><i className="mdi mdi-facebook-box"></i></a>
+            <a className="mdl-typography--display-3 social-link" href={this.props.twitterUrl} target="_blank"><i className="mdi mdi-twitter-box"></i></a>
+            <a className="mdl-typography--display-3 social-link" href={this.props.gPlusUrl} target="_blank"><i className="mdi mdi-google-plus-box"></i></a>
           </div>
         </div>
       </div>
