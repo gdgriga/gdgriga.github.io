630e7608db2de2a26a13b3e255ab218d22966df3
Ilya <karpov.ilja@gmail.com>
2016-10-16 01:37:04 +0300

    #3 Firebase integration

--- app/components/EventList.jsx
+++ app/components/EventList.jsx
@@ -3,14 +3,26 @@ import EventCard from './EventCard.jsx';
 
 class EventList extends React.Component {
   render() {
+    if (this.props.events.length === 0) {
+      return (
+        <div className="mdl-grid">
+          <div className="mdl-layout-spacer"></div>
+          <h6>No events here for now. Please check again later.</h6>
+          <div className="mdl-layout-spacer"></div>
+        </div>
+      );
+    }
+
     let eventNodes = this.props.events.map(event => {
+      this.checkBanner(event);
+
       return (
         <EventCard
-          key={event.link}
+          key={event.event_url}
           banner={event.banner}
           name={event.name}
-          description={event.description}
-          link={event.link}
+          description={event.description_no_html||event.description}
+          link={event.event_url}
           time={event.time} />
       );
     });
@@ -20,6 +32,16 @@ class EventList extends React.Component {
         {eventNodes}
       </div>
     );
+  }
+
+  checkBanner(event) {
+    if (event.banner) {
+      return;
+    }
+
+    let el = document.createElement('div');
+    el.innerHTML = event.description;
+    event.banner = el.getElementsByTagName("img")[0].src;
   }
 }
 
