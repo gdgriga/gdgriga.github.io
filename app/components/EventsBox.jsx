22c2fe507dd30afa726d674db0b007065236db5b
Sergey Kibish <sergey.kibish@gmail.com>
2016-10-21 20:10:04 +0300

    Remove load from file (because everythong is in firebase), optmize load, remove unused components

--- app/components/EventsBox.jsx
+++ app/components/EventsBox.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import EventsBoxFiles from './EventsBoxFiles.jsx';
-import EventsBoxFirebase from './EventsBoxFirebase.jsx';
+import EventList from './EventList.jsx';
 
 class EventsBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {data: []};
   }
 
   render() {
-    if (this.props.source === 'firebase') {
-      return (
-          <EventsBoxFirebase type={this.props.type}/>
-      );
-    } else if (this.props.source === 'files') {
-      return (
-          <EventsBoxFiles type={this.props.type} url="/storage/events.json"/>
-      );
-    }
+    return (
+      <div className="events-box">
+        <EventList events={this.props.events} />
+      </div>
+    );
   }
 }
 
