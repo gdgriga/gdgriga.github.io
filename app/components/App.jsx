519c79311fb3f39d4351110c8fdb9ba8732cf401
Sergey Kibish <skibish@users.noreply.github.com>
2016-10-23 15:24:46 +0300

    Remove load from file (because everythong is in firebase), optmize load, remove unused components (#13)

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -6,20 +6,52 @@ import TopBannerBox from './TopBannerBox.jsx';
 import HeadLine from './HeadLine.jsx';
 import SocialBox from './SocialBox.jsx';
 import About from './About.jsx';
+import * as firebase from 'firebase';
+
+firebase.initializeApp({
+  databaseURL: "https://gdg-riga-8ccd7.firebaseio.com"
+});
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {main: {}, social: {}};
+    this.state = {
+      main: {},
+      social: {},
+      events_upcoming: [],
+      events_past: [],
+    };
 
   }
 
   componentDidMount() {
+    // fetsh data from JSON file
     fetch(this.props.url)
     .then(response => response.json())
     .then(info => {
+      // Update initial state to fill all fetched info
       this.setState({main: info, social: info.social});
+    })
+    .then(state => {
+      // then fetch all data from the firebase ...
+      let self = this;
+
+      var now = (new Date()).getTime();
+      var ref = firebase.database().ref('events');
+
+      ref.once('value').then(function(snapshot) {
+        let events = snapshot.val();
+
+        if (events) {
+          // ... and update state
+          self.setState(Object.assign({}, self.state, {
+            events_upcoming: Object.values(events).filter(event => event.time >= now).sort((a, b) => b.time - a.time),
+            events_past: Object.values(events).filter(event => event.time <= now).sort((a, b) => b.time - a.time)
+          }));
+        }
+      });
+
     });
   }
 
@@ -51,10 +83,10 @@ class App extends React.Component {
             gPlusUrl={this.state.social.googlePlus} />
 
           <HeadLine title="Upcoming events" id="upcoming-events" />
-          <EventsBox source="firebase" type="upcoming-events"/>
+          <EventsBox events={this.state.events_upcoming} />
 
           <HeadLine title="Past events" id="past-events" />
-          <EventsBox source="firebase" type="past-events"/>
+          <EventsBox events={this.state.events_past} />
         </main>
       </div>
     );
