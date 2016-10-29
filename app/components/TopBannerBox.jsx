a8f5c2f99c2dfc2e0fcf558e8d984a4369cb5c7e
Ilya <karpov.ilja@gmail.com>
2016-10-30 01:38:45 +0300

    #4 added group events and participants numbers

--- app/components/TopBannerBox.jsx
+++ app/components/TopBannerBox.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Odometer from 'odometer';
 
 class TopBannerBox extends React.Component {
   render() {
     return (
-      <section className="jumbo mdl-cell mdl-cell--12-col mdl-typography--display-1">
-      </section>
+      <div className="jumbo headline mdl-cell mdl-cell--12-col mdl-grid mdl-typography--display-2 mdl-color-text--grey-50">
+        <div className="mdl-grid mdl-cell--12-col mdl-cell--bottom">
+          <div className="mdl-cell mdl-cell--4-col mdl-cell--2-offset">
+            <div className="odometer">{this.props.events_count}</div> events
+          </div>
+          <div className="mdl-cell mdl-cell--4-col">
+            <div className="odometer">{this.props.events_rsvp_count}</div> participants
+          </div>
+        </div>
+      </div>
     );
   }
 }
