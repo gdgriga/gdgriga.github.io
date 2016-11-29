22fb3068fae71ab85b18ae74cb953048f7f67842
Ilya <karpov.ilja@gmail.com>
2016-11-29 23:21:38 +0200

    Social buttons (#15)

--- app/components/SocialBox.jsx
+++ app/components/SocialBox.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
 class SocialBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    if(props.color) {
+      this.state = {
+        style: {
+          color: props.color
+        }
+      };
+    }
+  }
+
   render() {
 
     return (
-      <div className="social-section">
-        <div className="mdl-grid section--center">
-          <div className="mdl-cell mdl-cell--6-col">
-            <div className="mdl-typography--display-2 mdl-typography--font-thin">Follow us</div>
-              <p className="mdl-typography--headline mdl-typography--font-thin">Be ready for something new</p>
-          </div>
-          <div className="mdl-cell mdl-cell--6-col mdl-typography--text-center">
-            <a className="mdl-typography--display-3 social-link" href={this.props.facebookUrl} target="_blank"><i className="mdi mdi-facebook-box"></i></a>
-            <a className="mdl-typography--display-3 social-link" href={this.props.twitterUrl} target="_blank"><i className="mdi mdi-twitter-box"></i></a>
-            <a className="mdl-typography--display-3 social-link" href={this.props.gPlusUrl} target="_blank"><i className="mdi mdi-google-plus-box"></i></a>
-          </div>
-        </div>
-      </div>
+      <span>
+        <a style={this.state.style} className="mdl-typography--display-1 social-link" href={this.props.facebookUrl} target="_blank"><i className="mdi mdi-facebook-box"></i></a>
+        <a style={this.state.style} className="mdl-typography--display-1 social-link" href={this.props.twitterUrl} target="_blank"><i className="mdi mdi-twitter-box"></i></a>
+        <a style={this.state.style} className="mdl-typography--display-1 social-link" href={this.props.gPlusUrl} target="_blank"><i className="mdi mdi-google-plus-box"></i></a>
+      </span>
     );
   }
 }
