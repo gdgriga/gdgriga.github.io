0a3772f2de1771e8c9acf2fabedc64e41e15aea9
Ilya <karpov.ilja@gmail.com>
2016-12-11 23:31:29 +0200

    Counter and style fix

--- app/components/Counter.jsx
+++ app/components/Counter.jsx
@@ -3,16 +3,27 @@ import Odometer from 'odometer';
 
 class Counter extends React.Component {
   render() {
+
+    var letterStyle = {
+      color: '#3F51B5',
+      fontSize: '2.2em',
+      fontWeight: 'bold'
+    };
+
     return (
       <div className="mdl-grid maxwidth">
-          <div className="mdl-cell mdl-cell--6-col mdl-shadow--2dp">
+          <div className="mdl-cell mdl-cell--4-col mdl-cell--2-offset">
             <div className="mdl-card__title">
-              <h4 className="mdl-card__title-text"><span className="odometer">{this.props.events_count}</span> events</h4>
+              <p className="mdl-card__title-text" style={letterStyle}>
+                <span className="odometer">{this.props.events_count}</span>&nbsp;events
+              </p>
             </div>
           </div>
-          <div className="mdl-cell mdl-cell--6-col mdl-shadow--2dp">
+          <div className="mdl-cell mdl-cell--4-col">
             <div className="mdl-card__title">
-              <h4 className="mdl-card__title-text"><span className="odometer">{this.props.events_rsvp_count}</span> participants</h4>
+              <p className="mdl-card__title-text" style={letterStyle}>
+                <span className="odometer">{this.props.events_rsvp_count}</span>&nbsp;developers
+              </p>
             </div>
           </div>
       </div>
