5b068c706ee3ad333581e9f50e39fa51aa201426
Ilya <karpov.ilja@gmail.com>
2016-12-13 00:28:12 +0200

    #19 fix

--- app/components/Drawer.jsx
+++ app/components/Drawer.jsx
@@ -3,9 +3,20 @@ import Navigation from './Navigation.jsx';
 
 class Drawer extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  /* https://github.com/google/material-design-lite/issues/1246 */
+  handleClick() {
+    document.querySelector('.mdl-layout__obfuscator').classList.remove('is-visible');
+    document.querySelector('.mdl-layout__drawer').classList.remove('is-visible');
+  }
+
   render() {
     return (
-      <div className="mdl-layout__drawer">
+      <div className="mdl-layout__drawer" onClick={this.handleClick}>
         <span className="mdl-layout-title"><img src="/assets/images/gdg_icon.svg" height="30px;"/>{this.props.title}</span>
         <Navigation links={this.props.links} />
       </div>
