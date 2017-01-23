89b4ff3605ce39e0f91df2ae2d73707cdbec5697
Sergey Kibish <sergey.kibish@gmail.com>
2017-01-23 22:25:59 +0200

    Expand supporting text in EventCard

--- app/components/EventCard.jsx
+++ app/components/EventCard.jsx
@@ -18,7 +18,7 @@ class EventCard extends React.Component {
         <div className="mdl-card__title">
           <h2 className="mdl-card__title-text">{this.props.name}</h2>
         </div>
-        <div className="mdl-card__supporting-text">{this.props.description}</div>
+        <div className="mdl-card--expand mdl-card__supporting-text">{this.props.description}</div>
         <div className="mdl-card__actions mdl-card--border">
           <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href={this.props.link} target="_blank">More info</a>
         </div>
